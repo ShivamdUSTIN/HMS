d73fea9b1b5b04bb06417743768da92a50298bcf
Ubuntu <ubuntu@ip-172-31-8-117.ap-south-1.compute.internal>
2025-07-20 07:28:25 +0000

    Saving local changes before pull

--- hospital_mgnt/hospital_mgnt/settings.py
+++ hospital_mgnt/hospital_mgnt/settings.py
@@ -77,8 +77,7 @@ WSGI_APPLICATION = 'hospital_mgnt.wsgi.application'
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
+#         'NAME': BASE_DIR / 'db.sqlit#     }
 # }
 
 
